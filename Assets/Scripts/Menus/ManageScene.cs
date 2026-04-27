396b20c894e50c9c23e7f9d1bf6fce972348505b
JoeyAdv <jsadvinc@uci.edu>
2026-04-26 19:16:03 -0700

    added buttons for the rest of the scenes + quit in-editor

--- Assets/Scripts/Menus/ManageScene.cs
+++ Assets/Scripts/Menus/ManageScene.cs
@@ -3,17 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class ManageScene : MonoBehaviour
 {
+    // Name of scene to load into
     [SerializeField] private string sceneName;
 
+    // Loads the scene based on name set in editor
     public void LoadScene()
     {
         Debug.Log("Load Scene: " + sceneName);
         SceneManager.LoadScene(sceneName);
     }
 
+    // Quits game
     public void Quit()
     {
         Debug.Log("Quitting game");
-        Application.Quit();            
+
+        /* if in-editor, stops playing
+           if in real build, exits application */
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.ExitPlaymode();
+        }
+        else
+        {
+            Application.Quit();      
+        }
+      
     }
 }
