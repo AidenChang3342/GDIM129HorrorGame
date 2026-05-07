89e51b803a2d7e20d0557f50ee89e8debca018b2
JoeyAdv <jsadvinc@uci.edu>
2026-05-06 22:43:32 -0700

    keypad implemented

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -5,9 +5,36 @@ public class Keypad : MonoBehaviour
 {
     [SerializeField] private Text output;
     [SerializeField] private AudioClip[] buttonPressedSFX;
+    [SerializeField] private string answer;
     public void KeyPressed(int input)
     {
         AudioManager.instance.PlayRandomSFX(buttonPressedSFX, this.transform, 1f);
-        output.text += input.ToString();
+
+        if (output.text.Length < 4)
+        {
+            output.text += input.ToString();
+        }
+
+    }
+
+    public void Delete()
+    {
+        AudioManager.instance.PlayRandomSFX(buttonPressedSFX, this.transform, 1f);
+
+        output.text = "";
+    }
+
+    public void Check()
+    {
+        AudioManager.instance.PlayRandomSFX(buttonPressedSFX, this.transform, 1f);
+
+        if (output.text == answer)
+        {
+            Debug.Log("correct code");
+        }
+        else
+        {
+            output.text = "";
+        }
     }
 }
