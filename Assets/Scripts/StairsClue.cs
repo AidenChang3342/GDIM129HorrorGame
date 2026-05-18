595e95f92a5e71d291d0a340412bf2a16aa5ae1c
JoeyAdv <jsadvinc@uci.edu>
2026-05-17 22:54:42 -0700

    basic stairs clue implemented

---
+++ Assets/Scripts/StairsClue.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class StairsClue : MonoBehaviour
+{
+    [SerializeField] private AudioClip footstepSFX;
+    [SerializeField] private float initialDelay;
+
+    private void Start()
+    {
+        // play footsteps sfx after a delay
+        StartCoroutine(PlayFootsteps());
+    }
+
+    // coroutine to play footstep sfx after a couple seconds delay
+    private IEnumerator PlayFootsteps()
+    {
+        // wait for initial delay
+        yield return new WaitForSeconds(initialDelay);
+
+        // play footstep sfx
+        AudioManager.instance.PlaySFX(footstepSFX, this.transform, 1f);
+    }
+}
