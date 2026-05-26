0b27e6df3e71ce3b68876fac451ca45a9eb43d17
AidenChang3342 <changw1600@gmail.com>
2026-05-26 11:33:20 -0700

    asset implementation

--- Assets/Scripts/BedroomClue.cs
+++ Assets/Scripts/BedroomClue.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class BedroomClue : MonoBehaviour
@@ -7,10 +8,18 @@ public class BedroomClue : MonoBehaviour
     [SerializeField] private AudioClip[] UISoundClip;
     [SerializeField] private ItemData clueItem;
     [SerializeField] private List<string> dialogueLines;
+    [Header("Key")]
+    [SerializeField] private GameObject key;
+    [SerializeField] private float keyShowTime = 3f;
+
 
     // on mouse click, play pickup sfx and show dialogue, then add clue to inventory
     // add later: add gamemanager to save picked up clues (this prevents key from returning when player leaves and comes back) 
     // implement unlocking door
+    private void Start()
+    {
+        key.SetActive(false);
+    }
     private void OnMouseDown()
     {
         if (UIManager.instance.inventoryUI.isOpen)
@@ -19,16 +28,25 @@ public class BedroomClue : MonoBehaviour
             return;
         }
 
-        AudioManager.instance.PlaySFX(pickupSFX, this.transform, 0.5f);
-        
-        // show dialogue
-        DialogueManager.instance.StartDialogue(dialogueLines);
-
-        // add clue to inventory
-        InventoryManager.instance.AddItem(clueItem);
-
-        // remove object
-        gameObject.SetActive(false);
+        StartCoroutine(PickupRoutine());
     }
 
+    private IEnumerator PickupRoutine()
+    {
+        AudioManager.instance.PlaySFX(pickupSFX, transform, 0.5f);
+
+        key.SetActive(true);
+
+        DialogueManager.instance.StartDialogue(dialogueLines);
+        InventoryManager.instance.AddItem(clueItem);
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(keyShowTime);
+
+        key.SetActive(false);
+
+        gameObject.SetActive(false);
+    }
 }
