8f22cd76b79d49f8a2085660780fe67b6c1c5899
AidenChang3342 <changw1600@gmail.com>
2026-05-26 14:52:39 -0700

    changed bit of UI, also added more to scriptable asset

--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -10,10 +10,9 @@ public class InventorySlot : MonoBehaviour
     public void Setup(ItemData newItem)
     {
         item = newItem;
-        // this is only temporary, replace color with actual sprite
-        // itemImage.sprite = item.itemSprite
-        // once this is done, remember to change the itemData to include the sprite
-        itemImage.color = item.itemColor;
+        itemImage.sprite = item.itemSprite;
+        itemImage.preserveAspect = true;
+        itemImage.enabled = item.itemSprite != null;
     }
 
     public void OnClick()
